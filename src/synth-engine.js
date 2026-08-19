56f9d41d1c50c173fabb1199c4f7a309ad8775f6
dudududu144 <dudududi144@gmail.com>
2026-08-20 00:04:08 +0300

    chore: bump worklet cache-bust 961

--- src/synth-engine.js
+++ src/synth-engine.js
@@ -23,7 +23,7 @@ class SynthEngine {
     const self = this;
     let load;
     if (self.ctx.audioWorklet) {
-      load = self.ctx.audioWorklet.addModule('psysynth-worklet.js?v=950');
+      load = self.ctx.audioWorklet.addModule('psysynth-worklet.js?v=961');
     } else {
       self.fallbackMode = true; self.node = null; load = Promise.resolve();
     }
