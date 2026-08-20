1e5c590017276c5c31f086cd22a6aad010bc8ab5
dudududu144 <dudududi144@gmail.com>
2026-08-20 10:54:08 +0300

    chore: bump worklet 1031

--- src/synth-engine.js
+++ src/synth-engine.js
@@ -23,7 +23,7 @@ class SynthEngine {
     const self = this;
     let load;
     if (self.ctx.audioWorklet) {
-      load = self.ctx.audioWorklet.addModule('psysynth-worklet.js?v=1022');
+      load = self.ctx.audioWorklet.addModule('psysynth-worklet.js?v=1031');
     } else {
       self.fallbackMode = true; self.node = null; load = Promise.resolve();
     }
